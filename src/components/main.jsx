4b946a6943a3dfd1833eea1b38f954e92e59e438
manidf <mannuel.ferreira@gmail.com>
2016-01-09 00:04:46 +0200

    header in plece with counter to show render

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,9 +1,15 @@
 var React = require('react');
+var Header = require('./header');
+
+var renderCount = 0;
 
 module.exports = React.createClass({
 	render: function() {
+		console.log(renderCount++);
 		return <div>
-			I am a header
+
+			<Header />
+
 			{this.props.children}
 		</div>
 	}
