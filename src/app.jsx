451226118edf2c5f9671b1c0a52eacc9cdae99f3
manidf <mannuel.ferreira@gmail.com>
2016-01-08 23:51:20 +0200

    header component created

--- src/app.jsx
+++ src/app.jsx
@@ -1,36 +1,5 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var ReactRouter = require('react-router');
-var Router = ReactRouter.Router;
-var Route = ReactRouter.Route;
-
-var Hello = React.createClass({
-  render: function() {
-    return <h1 className="red">
-			{this.props.children}
-    </h1>
-  }
-});
-
-var Child1 = React.createClass({
-	render: function() {
-		return <h1>I'm a child element 1</h1>
-	}
-});
-
-var Child2 = React.createClass({
-	render: function() {
-		return <h1>I'm a child element 2</h1>
-	}
-});
-
-var Routes = (
-	<Router>
-		<Route path="/" component={Hello}>
-			<Route path="1" component={Child1} />
-			<Route path="2" component={Child2} />
-		</Route>
-	</Router>
-);
+var Routes = require('./routes');
 
 ReactDOM.render(Routes, document.querySelector('.container'));
